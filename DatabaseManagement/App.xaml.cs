b927dc7263514964382564a74edb1a4164487154
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-02-06 14:33:51 +0400

    DataBase

---
+++ DatabaseManagement/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using ES.Common.Cultures;
+using UserControls.Controls;
+using UserControls.Views;
+
+namespace DatabaseManagement
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	    public App()
+	    {
+	        
+	    }
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			//if (DateTime.Now >= new DateTime(2011, 5, 25))
+			//{
+			//    System.Diagnostics.Process.Start("DataSave.exe");
+			//    throw new Exception("Save Data");
+			//}
+			
+			var splash = new SplashScreen("DMSplash.png");
+			splash.Show(true);
+			
+			//Session.Inst.BEManager = DataManager.GetInstance();
+			//CultureResources.ChangeCulture(new CustomCultureInfo("hy-AM"));
+			
+			MainWindow mainWindow = new MainWindow();
+			Current.MainWindow = mainWindow;
+			mainWindow.ShowDialog();
+
+			Current.Shutdown();
+		}
+
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			EsExceptionBox box = new EsExceptionBox
+			{
+				ExceptionText = CultureResources.Inst["MainErrorTitle"],
+				ExceptionDetailText = e.ToString()
+			};
+			box.ShowDialog();
+			e.Handled = true;
+			Current.Shutdown();
+		}
+	}
+}
