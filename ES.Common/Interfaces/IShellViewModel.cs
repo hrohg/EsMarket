3c698de4dd64dc5e1799a3b1042b2e5de3ea39ef
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-02-07 17:04:46 +0400

    OK

---
+++ ES.Common/Interfaces/IShellViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using ES.Common.Enumerations;
+
+namespace ES.Common.Interfaces
+{
+    public interface IShellViewModel: INotifyPropertyChanged
+    {
+        void OnGetInvoices(object o);
+        void OnGetReport(ReportTypes type);
+    }
+}
