4dc9d240ca29339da8b8150e1dfb5419ee2a915d
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-02-07 17:05:15 +0400

    OK

---
+++ UserControls/Controls/UctrlMyTime.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using UserControls.ViewModels.Invoices;
+using UserControls.ViewModels.Reports;
+
+namespace UserControls.Controls
+{
+    /// <summary>
+    /// Interaction logic for UctrlMyTime.xaml
+    /// </summary>
+    public partial class UctrlMyTime : UserControl
+    {
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Time", typeof(MyTime), typeof(UserControl), null);
+        public MyTime Time
+        {
+            //get { return (MyTime)GetValue(TimeProperty); }
+            get { return DataContext as MyTime;}
+            set { SetValue(TimeProperty, value); }
+        }
+        //public MyTime Time
+        //{
+        //    get
+        //    {
+        //        return (MyTime)DataContext;
+        //    }
+        //    set { DataContext = value; }
+        //}
+        public UctrlMyTime()
+        {
+            InitializeComponent();
+            DataContext = Time = new MyTime();
+        }
+        public UctrlMyTime(MyTime time)
+        {
+            InitializeComponent();
+            DataContext = Time = time;
+        }
+    }
+}
---
+++ UserControls/ViewModels/Documents/StartPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Input;
+using ES.Common.Enumerations;
+using ES.Common.Helpers;
+using ES.Common.Interfaces;
+using ES.Common.ViewModels.Base;
+
+namespace UserControls.ViewModels.Documents
+{
+    public class StartPageViewModel : DocumentViewModel
+    {
+        #region Internal properties
+
+        private readonly IShellViewModel _parent;
+        #endregion Internal properties
+
+        #region External properties
+
+        #endregion External properties
+
+        #region Constructors
+
+        public StartPageViewModel(IShellViewModel parent)
+        {
+            _parent = parent;
+            Initialize();
+        }
+        #endregion Constructors
+
+        #region Internal methods
+
+        private void Initialize()
+        {
+            Title = "Գլխավոր";
+            CanFloat = false;
+            IsClosable = false;
+            OpenInvocieCommand = new RelayCommand(OnOpenInvoice);
+            GetReportCommand = new RelayCommand<ReportTypes>(OnGetReport);
+        }
+
+        private void OnOpenInvoice(object o)
+        {
+            _parent.OnGetInvoices(o);
+        }
+
+        private void OnGetReport(ReportTypes type)
+        {
+            _parent.OnGetReport(type);
+        }
+        #endregion Internal methods
+
+        #region External methods
+
+        #endregion External methods
+
+        #region Commands
+
+        #region Invoices
+        public ICommand OpenInvocieCommand { get; private set; }
+        #endregion Invoices
+
+        #region Reports
+        public ICommand GetReportCommand { get; private set; }
+        #endregion Reports
+
+        #endregion Commands
+    }
+}
