715a498da30e86d3ed5e9def17da438e6e0f3739
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-02-07 17:04:58 +0400

    OK

---
+++ ES.Common/ViewModels/Base/DocumentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ES.Common.ViewModels.Base
+{
+    public class DocumentViewModel : PaneViewModel
+    {
+        #region Internal properties
+
+        #endregion Internal properties
+
+        #region External properties
+
+        #endregion External properties
+
+        #region Constructors
+
+        public DocumentViewModel()
+        {
+            IsClosable = true;
+        }
+        #endregion Constructors
+
+        #region Internal methods
+        #endregion Internal methods
+
+    }
+}
---
+++ ES.Common/ViewModels/Base/ToolsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Xceed.Wpf.AvalonDock.ExtendedAvalonDock.Interfaces;
+using Xceed.Wpf.AvalonDock.Layout;
+
+namespace ES.Common.ViewModels.Base
+{
+    public class ToolsViewModel : PaneViewModel, IExtendedAnchorableBase
+    {
+        #region External properties
+        public AnchorSide AnchorSide { get; set; }
+        #endregion External properties
+    }
+}
