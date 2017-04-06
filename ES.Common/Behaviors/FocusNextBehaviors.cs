dae0154f5cffec70fb8b7445844733ad1350f131
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-04-06 10:46:20 +0400

    ok

---
+++ ES.Common/Behaviors/FocusNextBehaviors.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace ES.Common.Behaviors
+{
+    public class FocusNextElementBehaviors : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += AssociatedObjectLostKeyDown;
+        }
+
+        private void AssociatedObjectLostKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            e.Handled = true;
+            var request = new TraversalRequest(FocusNavigationDirection.Next) { Wrapped = false };
+            AssociatedObject.MoveFocus(request);
+
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.KeyDown -= AssociatedObjectLostKeyDown;
+            base.OnDetaching();
+        }
+    }
+}
