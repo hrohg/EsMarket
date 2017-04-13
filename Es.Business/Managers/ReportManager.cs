1808301a3828a3b51385e83cb95ad5e0564203f7
Hrayr Gishyan <hrayrgishyan@gmail.com>
2017-04-14 00:06:39 +0400

    ok

---
+++ Es.Business/Managers/ReportManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CashReg.Helper;
+using ES.Business.Helpers;
+using ES.Data.Models;
+using ES.DataAccess.Models;
+
+namespace ES.Business.Managers
+{
+    public class ReportManager : BaseManager
+    {
+        public static List<InvoiceModel> GetShortInvoiceReport(DateTime startDate, DateTime endDate)
+        {
+            using (var db = GetDataContext())
+            {
+                try
+                {
+                    return
+                        db.Invoices.Where(s => s.MemberId == ApplicationManager.Instance.GetEsMember.Id && s.ApproveDate != null && s.ApproveDate >= startDate && s.ApproveDate <= endDate)
+                            .Select(s => new InvoiceModel
+                            {
+                                Id = s.Id,
+                                InvoiceTypeId = (int)s.InvoiceTypeId,
+                                CreateDate = s.CreateDate,
+                                ApproveDate = s.ApproveDate.Value,
+                                InvoiceNumber = s.InvoiceNumber,
+                                //CostPrice = s.InvoiceItems.Sum(i => i.Quantity * i.CostPrice) ?? 0,
+                                Total = s.Summ,
+                                Notes = s.Notes,
+                                PartnerId = s.PartnerId,
+                                ApproverId = s.ApproverId.Value,
+                                MemberId = s.MemberId,
+                                Approver = s.Approver,
+                                Creator = s.Creator
+                            }).ToList();
+                }
+                catch (Exception)
+                {
+                    return new List<InvoiceModel>();
+                }
+            }
+        }
+    }
+}
