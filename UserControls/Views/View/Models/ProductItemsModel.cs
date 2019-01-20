5fdb5d9cbff1577af337f8adf0d2714036b3fedb
Hrayr Gishyan <hrayrgishyan@gmail.com>
2019-01-20 13:15:38 +0400

    ok

---
+++ UserControls/Views/View/Models/ProductItemsModel.cs
@@ -0,0 +1,11 @@
+﻿using ES.Data.Models;
+
+namespace UserControls.Views.View.Models
+{
+    public class ProductItemsModel
+    {
+        public ProductItemModel ProductItem { get; set; }
+        public ProductModel Product { get; set; }
+        public StockModel Stock { get; set; }
+    }
+}
