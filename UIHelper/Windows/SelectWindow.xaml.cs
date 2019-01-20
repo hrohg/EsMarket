424e2244669d3292d1068f06e43053437a8b09b3
Hrayr Gishyan <hrayrgishyan@gmail.com>
2019-01-20 13:17:57 +0400

    2019 January

---
+++ UIHelper/Windows/SelectWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using UIHelper.Interfaces;
+
+namespace UIHelper.Windows
+{
+    /// <summary>
+    /// Interaction logic for SelectDateIntermediate.xaml
+    /// </summary>
+    public partial class SelectWindow : Window
+    {
+        public SelectWindow(ISelectable vm)
+        {
+            DataContext = vm;
+            InitializeComponent();
+        }
+
+        #region Events
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+        #endregion
+    }
+}
