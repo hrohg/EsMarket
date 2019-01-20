f2495e5a568656ac0765c0992f9d786b26fca771
Hrayr Gishyan <hrayrgishyan@gmail.com>
2019-01-20 13:03:52 +0400

    ok

---
+++ ES.Data/Converters/EsMarketInvoiceConverter.cs
@@ -0,0 +1,24 @@
+﻿using EsMarket.SharedData.Interfaces;
+using EsMarket.SharedData.Models;
+using ES.Data.Models;
+
+namespace ES.Data.Converters
+{
+    public class EsMarketInvoiceConverter
+    {
+        public static EsMarketInvoice ConvertToEsMarketInvoice(InvoiceModel invoice, InvoiceItemsModel invoiceItem)
+        {
+            EsMarketInvoice newItem = null;
+            return newItem;
+        }
+        public static InvoiceModel ConvertToInvoiceModel(EsMarketInvoice esMarketInvoice)
+        {
+            var newItem = new InvoiceModel()
+            {
+                
+            };
+            return newItem;
+        }
+    }
+    
+}
---
+++ ES.Data/Models/StockProducts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace ES.Data.Models
+{
+    public class StockProducts : IComparable
+    {
+        public StockModel Stock { get; set; }
+        public ProductItemModel Product { get; set; }
+        public decimal Quantity { get; set; }
+
+        public int CompareTo(StockProducts other)
+        {
+            if (other == null) return 0;
+            return Quantity > other.Quantity ? 1 : -1;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var o = obj as StockProducts;
+            if (o == null) return 1;
+
+            return Quantity >= o.Quantity ? 1 : -1;
+        }
+    }
+}
