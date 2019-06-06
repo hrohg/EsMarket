d79f26cbf5a332339d25696c319bdea4eda08758
Hrayr Gishyan <hrayrgishyan@gmail.com>
2019-06-06 17:59:17 +0400

    added productitemviewmodel

---
+++ UserControls/Views/Products/ProductItemsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ES.Common.ViewModels.Base;
+
+namespace UserControls.Views.Products
+{
+    public class ProductItemsViewModel:ViewModelBase
+    {
+        #region Internal properties
+
+        private decimal _count;
+        
+        #endregion
+
+        #region External properties
+        #endregion
+
+        #region Constructors
+
+        public ProductItemsViewModel()
+        {
+
+        }
+        #endregion
+    }
+}
