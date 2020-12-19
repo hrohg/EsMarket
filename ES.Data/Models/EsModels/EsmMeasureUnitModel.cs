44bfe8d533a15c795620ed9b894b21ea1d83e6fa
Hrayr Gishyan <hrayrgishyan@gmail.com>
2020-12-20 01:31:27 +0400

    added missed files

---
+++ ES.Data/Models/EsModels/EsmMeasureUnitModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ES.Data.Models.EsModels
+{
+    public class EsmMeasureUnitModel
+    {
+        public short Id { get; set; }
+        public int? DisplayOrder { get; set; }
+        public string Key { get; set; }
+        public string KeyValue { get { return Key; } }
+        public string Name { get; set; }
+        public string NameValue { get { return Key; } }
+        public decimal? Ratio { get; set; }
+        public bool? IsWeight { get; set; }
+        public int? GroupId { get; set; } 
+    }
+}
