6f34fb5b7e7a1d6b028a3331fc5e42494c0aa528
Hrayr Gishyan <hrayrgishyan@gmail.com>
2020-12-20 01:29:50 +0400

    Added missed files

---
+++ ES.Data/Models/Products/ProductModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+using ES.Data.Models.EsModels;
+
+namespace ES.Data.Models.Products
+{
+    public partial class ProductModel
+    {
+        private EsmMeasureUnitModel _measureUnit;
+        public EsmMeasureUnitModel MeasureUnit { get { return _measureUnit; } set { _measureUnit = value; } }
+        public int? MeasureUnitId { get { return MeasureUnit!=null? MeasureUnit.Id:(int?)null; } }
+
+        [XmlIgnore]
+        public string Mu { get { return MeasureUnit != null ? MeasureUnit.Name : string.Empty; } }
+        [XmlIgnore]
+        public bool IsWeight { get { return MeasureUnit != null && (MeasureUnit.IsWeight ?? false); } }
+
+
+    }
+}
---
+++ ES.Data/Models/Products/ProductsAdditionalDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using CashReg.Helper;
+
+namespace ES.Data.Models.Products
+{
+    public partial class ProductModel
+    {
+        [XmlIgnore]
+        public TypeOfTaxes TypeOfTaxes { get; set; }
+        
+    }
+}
