337ba43770307ef3857a301ca440956d77b8b981
Hrayr <hrayrgishyan@gmail.com>
2024-06-30 14:56:14 +0400

    rel 0.2 Jul 24 missing files.

---
+++ ES.Data/Models/Invoices/InvoiceItemAdditionalData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ES.Data.Models.Invoices
+{
+    public delegate void OnDataChanged();
+
+    [Serializable]
+    public class InvoiceItemAdditionalData
+    {
+
+        public event OnDataChanged DataChanged;
+        public ObservableCollection<string> EMarks { get; set; }
+
+        public InvoiceItemAdditionalData()
+        {
+            EMarks = new ObservableCollection<string>();
+            EMarks.CollectionChanged += (object sender, NotifyCollectionChangedEventArgs e) => { OnDataChanged(); };
+        }
+        private void OnDataChanged()
+        {
+            var handler = DataChanged;
+            if (handler != null)
+            {
+                handler();
+            }
+        }
+    }
+}
---
+++ Es.Business/Managers/EssManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ES.Business.Managers
+{
+    public enum LicensePlansEnum
+    {
+        None, Starter, Advanced
+    }
+    public class LicensePlanModel
+    {
+        public bool CanRedeemVaucher { get; set; }
+    }
+    public class EsMarketManager
+    {
+        private Dictionary<LicensePlansEnum, LicensePlanModel> _licensePlans;
+        private static EsMarketManager _instance;
+        public static EsMarketManager Instance { get { return _instance ?? (_instance = new EsMarketManager()); } }
+        EsMarketManager()
+        {
+            _licensePlans = new Dictionary<LicensePlansEnum, LicensePlanModel>();
+            Initialize();
+        }
+        private void Initialize()
+        {
+            _licensePlans.Add(LicensePlansEnum.Starter, new LicensePlanModel { });
+            _licensePlans.Add(LicensePlansEnum.Advanced, new LicensePlanModel { CanRedeemVaucher = true }); ;
+        }
+        public LicensePlanModel GetLicensePlan(LicensePlansEnum license)
+        {
+            return _licensePlans.ContainsKey(license) ? _licensePlans[license] : new LicensePlanModel();
+        }       
+
+        public static LicensePlansEnum GetLicense(int memberId)
+        {
+            return LicensePlansEnum.Advanced;
+        }
+
+        public object GetLicensePlan(object getLicense)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
