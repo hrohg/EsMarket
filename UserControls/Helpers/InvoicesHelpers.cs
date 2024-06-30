8627412f49f9b4fda5001610b784a8f81e2d57aa
Hrayr <hrayrgishyan@gmail.com>
2024-06-30 17:39:26 +0400

    rel_0.2

---
+++ UserControls/Helpers/InvoicesHelpers.cs
@@ -0,0 +1,18 @@
+﻿using UserControls.ControlPanel.Controls;
+
+namespace UserControls.Helpers
+{
+    public class InvoicesHelpers
+    {
+        public static string ReadEmark(string description, string emark = null)
+        {
+            //var inputWindow = new InputBox(description) { Title = "Հսկիչ նշանի ընթերցում", InputValue = emark };
+            //var inputWindow = Ecr.Manager.Helpers.MarkHelper.ReadEmark(description, emark);
+            //inputWindow.ShowDialog();
+            //if (inputWindow.DialogResult != true) return null;
+            //emark = inputWindow.InputValue;
+            //return emark;
+            return Ecr.Manager.Helpers.MarkHelper.ReadEmark(description, emark);
+        }
+    }
+}
